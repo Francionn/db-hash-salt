d5031f12922cc0dc9a779adf28e00a1ffb8f3487
Francion Magalhães <francion.magalhaes1@gmail.com>
2024-06-17 15:00:25 -0300

    Delete data_base_hash_salt directory

--- data_base_hash_salt/app/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from infra.repository.user_repository import User_repository
-
-
-repository = User_repository()
-app = Flask(__name__)
-
-from app.controllers import default
--- data_base_hash_salt/app/controllers/form_register.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
-from wtforms.validators import DataRequired, EqualTo
-
-class RegisterUser(FlaskForm):
-    name = StringField("name", validators=[DataRequired()])
-    email = StringField("email", validators=[DataRequired()])
-    password = PasswordField("password", validators=[DataRequired()])
-    password_confirm = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password', message='Passwords must match')])
