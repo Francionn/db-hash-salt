e7195dc3ee2fe62d7e1153b4705d4da05ca0aa36
Francion Magalhães <francion.magalhaes1@gmail.com>
2024-06-17 14:58:10 -0300

    Delete run.py

--- run.py
+++
@@ -1,9 +0,0 @@
-from infra.repository.user_repository import User_repository
-
-if __name__ == "__main__":
-    repository = User_repository()
-
-    repository.create_user("Data base with hash salt","@giithub@","github@example.com")
-    repository.login("git_hub@example.com", "@giithub@")
-    repository.delete(1)
-
